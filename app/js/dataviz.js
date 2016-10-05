2d314ac7c0d2f7cab566c52c1e9fa374641661ad
bogdanp <bogdan.pestritu@rga.com>
2016-10-05 16:08:48 +0300

    trying to get the data to show?

--- app/js/dataviz.js
+++ app/js/dataviz.js
@@ -7,36 +7,45 @@ let newData = [],
     timeParse = d3.timeParse("%d-%m-%Y %H:%M:%S"); //31-12-2015 10:31:32
 
 // recreate data
-for(let i = 0; i < data.length; i++) {
-  let obj = {
-    date: timeParse(data[i]["Data"]),
-    values: {
-      "ape": data[i]["Ape[MW]"],
-      "biomasa": data[i]["Biomasa[MW]"],
-      "carbune": data[i]["Carbune[MW]"],
-      "consum": data[i]["Consum[MW]"],
-      "eolian": data[i]["Eolian[MW]"],
-      "foto": data[i]["Foto[MW]"],
-      "hidrocarburi": data[i]["Hidrocarburi[MW]"],
-      "media_consum": data[i]["Medie Consum[MW]"],
-      "nuclear": data[i]["Nuclear[MW]"],
-      "productie": data[i]["Productie[MW]"],
-      "sold": data[i]["Sold[MW]"]
-    }
-  };
-  newData.push(obj);
-}
-//193.226.136.172
-//5.2.158.169
-//170.200.71.4
-console.log("usable data: ", newData);
+// for(let i = 0; i < data.length; i++) {
+//   let obj = {
+//     date: timeParse(data[i]["Data"]),
+//     values: {
+//       "ape": data[i]["Ape[MW]"],
+//       "biomasa": data[i]["Biomasa[MW]"],
+//       "carbune": data[i]["Carbune[MW]"],
+//       "consum": data[i]["Consum[MW]"],
+//       "eolian": data[i]["Eolian[MW]"],
+//       "foto": data[i]["Foto[MW]"],
+//       "hidrocarburi": data[i]["Hidrocarburi[MW]"],
+//       "media_consum": data[i]["Medie Consum[MW]"],
+//       "nuclear": data[i]["Nuclear[MW]"],
+//       "productie": data[i]["Productie[MW]"],
+//       "sold": data[i]["Sold[MW]"]
+//     }
+//   };
+//   newData.push(obj);
+// }
+
+data.forEach((d) => {
+  d.Data = timeParse(d.Data);
+});
+
+var dataByDate = d3.nest()
+  .key((d) => d.Data)
+  .entries(data);
+
+console.log("usable data: ", dataByDate);
 
 let stack = d3.stack()
-  .keys(["ape", "biomasa", "carbune", "consum", "eolian", "foto", "hidrocarburi", "media_consum", "nuclear", "productie", "sold"])
-  .value((d) => d.values)
+  .keys(["Ape[MW]", "Biomasa[MW]", "Carbune[MW]", "Consum[MW]", "Eolian[MW]", "Foto[MW]", "Hidrocarburi[MW]", "Medie Consum[MW]", "Nuclear[MW]", "Productie[MW]", "Sold[MW]"])
+  .value((d) => {console.log(d.values); return d.values;})
   .order(d3.stackOrderInsideOut)
   .offset(d3.stackOffsetWiggle);
-let layers0 = stack(newData);
+
+let layers = stack(dataByDate);
+
+console.log("layers: ", layers);
 
 var width = 960,
     height = 480;
@@ -46,7 +55,7 @@ var x = d3.scaleTime()
     .range([0, width]);
 
 var y = d3.scaleLinear()
-    .domain([0, d3.max(layers0, function(layer) { return d3.max(layer, function(d) { console.log(d);return d[0]; }); })])
+    .domain([0, d3.max(newData, function(d) { return d.y0 + d.y; })])
     .range([0, height]);
 
 var color = d3.scaleLinear()
@@ -54,7 +63,8 @@ var color = d3.scaleLinear()
 
 var area = d3.area()
     .x(function(d) {
-      return x(d[0]);
+      console.log("from area: ", d);
+      return x(d.date);
     })
     .y0(function(d) {
       return y(d.y0);
@@ -69,7 +79,7 @@ var svg = d3.select(".cm-dataviz")
     .attr("height", height);
 
 svg.selectAll("path")
-    .data(layers0)
+    .data(layers)
     .enter()
     .append("path")
     .attr("d", area)
